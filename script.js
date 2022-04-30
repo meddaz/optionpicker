efc16db4fdb5b1e7c26cc201f68d75881e441a26
meddaz <40867923+meddaz@users.noreply.github.com>
2022-04-30 08:17:00 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+randomize.addEventListener('click', randomItem);
+
+function randomItem() {
+var random=list.value.split(',', '\n');
+) 
+  
+  
+  
+ display.innerHTML = random[Math.floor(Math.random() * random.length)];
+  
+}
